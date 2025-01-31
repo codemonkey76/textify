b3bf16075b9531fdcaf9a07110e2a44f18d65aed
Shane Poppleton <shane@bjja.com.au>
2025-01-31 12:56:48 +1000

    Add better logging of sns messages

--- app/Http/Controllers/AwsSnsController.php
+++ app/Http/Controllers/AwsSnsController.php
@@ -10,7 +10,7 @@ class AwsSnsController extends Controller
 {
     public function __invoke(Request $request)
     {
-        Log::info("Receieved SNS Notification", ['request' => $request->all()]);
+        Log::info("Receieved SNS Notification", ['raw_body' => $request->getContent(), 'headers' => $request->headers->all()]);
         return response()->json(['message' => 'Received'], Response::HTTP_OK);
     }
 }
