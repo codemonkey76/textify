3b2cdf699e6e8efeba781efcbe2fc8fb6c6280e7
Shane Poppleton <shane@bjja.com.au>
2025-02-03 09:07:56 +1000

    Update CSRF exceptions to include delivery

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: ['/inbound', '/sns']);
+        $middleware->validateCsrfTokens(except: ['/inbound', '/sns', '/delivery']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
