58528a00952f825cc36bf73504c9cb709f7e500f
Shane Poppleton <shane@bjja.com.au>
2025-02-04 11:09:57 +1000

    Update vite config

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Login</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/js/app.ts'])
 </head>
 <body class="h-full">
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
