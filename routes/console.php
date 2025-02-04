f613d998714ae7c4cdbc52a81631cfb3f1b4b303
Shane Poppleton <shane@bjja.com.au>
2025-02-04 14:56:40 +1000

    add back missing routes

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('telescope:prune')->daily();
+Schedule::command('telescope:prune')->daily();
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AwsSnsController;
+use App\Http\Controllers\SmsDeliveryController;
+use App\Http\Controllers\WebhookController;
+use App\Http\Middleware\EnsureSingleWavAttachment;
+use App\Http\Middleware\VerifyAwsSnsSignature;
+use App\Http\Middleware\VerifyClickSendSignature;
+use App\Http\Middleware\VerifyWebhookSignature;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,5 +16,21 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('auth.login');
 })->middleware('guest')->name('login.form');
+
+Route::post('/inbound', WebhookController::class)
+    ->middleware([
+        VerifyWebhookSignature::class,
+        EnsureSingleWavAttachment::class
+    ]);
+
+Route::post('/sns', AwsSnsController::class)
+    ->middleware([
+        VerifyAwsSnsSignature::class
+    ]);
+
+Route::post('/delivery', SmsDeliveryController::class)
+    ->middleware([
+        VerifyClickSendSignature::class
+    ]);
 
 require __DIR__ . '/auth.php';
